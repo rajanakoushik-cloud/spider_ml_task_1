c09385027f6ff432dbf52474eb908617735524ee
rajanakoushik-cloud <rajanakoushik@gmail.com>
2026-06-10 21:12:25 +0530

    Update code.py

--- spider_ml_task_1/applied_ml/code.py
+++ spider_ml_task_1/applied_ml/code.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 token = "tokens"
 if not token:
-    st.error("Missing Hugging Face Token! Make sure your .env file contains tokens=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")
+    st.error("Missing Hugging Face Token! Make sure your .env file contains tokens=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")
     st.stop()
 
 st.title("AI Assistant CHITTI")
